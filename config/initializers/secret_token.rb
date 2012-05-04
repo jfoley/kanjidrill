9eeba2d35cab9b192a36078f61c255266b530a60
John Foley <john@thefol.io>
2012-05-03 21:22:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KanjiDrill::Application.config.secret_token = '580522cab824f62c444441226b59b06d48438530e1a962d160a79889a7934efd6c72364b8a983033d3836c5577276c43a31706bf47b4ff0137c47e4a06374169'
